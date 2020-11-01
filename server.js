9a579542c71d8075626df78afe598e4ffe35c287
Seth Uschuk <suschuk24@gmail.com>
2020-11-01 12:17:12 -0600

    created homepage and main handlebars pages

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-// const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 // // const helpers = require('./utils/helpers');
-// const hbs = exphbs.create({});
+const hbs = exphbs.create({});
 // const session = require('express-session');
 // const SequelizeStore = require('connect-session-sequelize')(session.Store);
 // const sess = {
@@ -17,18 +17,18 @@ const sequelize = require('./config/connection');
 // };
 
 
-// const path = require('path');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(session(sess));
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
